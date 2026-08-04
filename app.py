eba3d5744191cf091e15806f0c2c42a0996a126b
simran rawat <simranrawat34912@gmail.com>
2026-08-04 21:57:00 +0530

    blur green deployment

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>Blue Environment</h1>
+    <h2>Version 1.0<h2>
+    <p>Application is running successfully in the blue environment
+    what's app simu.</p>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
